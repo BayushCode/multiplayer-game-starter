40dcaaf1eeb37749d73ee72c7b18f152c471c8f9
BayushCode <hiayush21@gmail.com>
2024-11-21 18:59:05 +0000

    g

--- public/js/index.js
+++ public/js/index.js
@@ -11,7 +11,7 @@ canvas.height = innerHeight
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player1 = new Player(x, y, 10, 'white')
+
 const players = {}
 
 socket.on('updatePlayers', (backendPlayers) => {
@@ -37,8 +37,6 @@ function animate() {
     const player = player[id]
     player.draw()
   }
-  player1.draw()
-  player2.draw()
 }
 
 animate()
