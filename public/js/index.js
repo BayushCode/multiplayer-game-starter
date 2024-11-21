a9444e82e03dc3f733d46250a2af42f81696d185
BayushCode <hiayush21@gmail.com>
2024-11-21 04:40:34 +0000

    h

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-var socket = io();
+const socket = io();
 
 const scoreEl = document.querySelector('#scoreEl')
 
@@ -12,7 +12,19 @@ const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 10, 'white')
+const players = {}
 
+socket.on('updatePlayers', (backendPlayers) => {
+  for (const id in backendPlayers)
+  {
+    const backendPlayer = backendPlayers[id]
+
+    if (!players[id])
+    {
+      players[id] = new Player(backendPlayer.x, backendPlayer.y, 10, 'white')
+    }
+  }
+})
 
 let animationId
 
@@ -20,8 +32,13 @@ function animate() {
   animationId = requestAnimationFrame(animate)
   c.fillStyle = 'rgba(0, 0, 0, 0.1)'
   c.fillRect(0, 0, canvas.width, canvas.height)
+  c.fillStyle(255,0,0)
 
-  player.draw()
+  for (const id in players)
+  {
+    const player = player[id]
+    player.draw()
+  }
 }
 
 animate()
