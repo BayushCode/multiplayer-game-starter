1f2e7363ded5ae2f4663295b945340cc566b98bc
BayushCode <hiayush21@gmail.com>
2024-11-22 03:15:31 +0000

    h

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -19,11 +19,12 @@ socket.on('updatePlayers', (backendPlayers) => {
     const backendPlayer = backendPlayers[id]
 
     if (!players[id]) {
-      players[id] = new Player({x:backendPlayer.x,
+      /*players[id] = new Player({x:backendPlayer.x,
         y:backendPlayer.y,
         radius:10,
-        color:'white'})
-    }
+        color:'white'})*/
+        players[id] = new Player(backendPlayer.x,backendPlayer.y,10,'white')
+      }
   }
 
   for (const id in players)
